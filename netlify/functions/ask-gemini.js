00a79f495441ad5ad967c491574ae482e2497093
ptmyduyen2106-abc <ptmyduyen2106@gmail.com>
2026-05-03 13:01:35 +0700

    Update ask-gemini.js

--- netlify/functions/ask-gemini.js
+++ netlify/functions/ask-gemini.js
@@ -2,7 +2,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 exports.handler = async (event) => {
   const genAI = new GoogleGenerativeAI(process.env.COFFEE_CARE);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
   
   try {
     const { prompt } = JSON.parse(event.body);
